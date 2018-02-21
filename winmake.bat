6e3af9b7e3ba0f19e49c64ebefc37c9f317099df
Peter Ferrie <peter.ferrie@gmail.com>
2018-02-21 15:48:03 -0800

    update winmake to match makefile

--- winmake.bat
+++ winmake.bat
@@ -1,35 +1,13 @@
 @echo off
 if "%1" equ "clean" (
+:clean
 echo y|1>nul 2>nul rd build /s
 goto :EOF
 )
 
-2>nul md build
 if "%1" equ "asm" (
-call :asm
-goto :EOF
-)
-
-set DISK=PitchDark.2mg
-
-if "%1" equ "dsk" (
-call :asm
-call :dsk
-set DISK=
-goto :EOF
-)
-
-if "%1" equ "txt" (
-call :asm
-call :dsk
-call :txt
-set DISK=
-goto :EOF
-)
-
-goto :EOF
-
 :asm
+2>nul md build
 acme src\grue.system.s
 acme src\pitchdark.a
 acme src\onbeyond\onbeyond.system.s
@@ -38,34 +16,68 @@ acme src\onbeyond\z4\z4.s
 acme src\onbeyond\z5\z5.s
 acme src\onbeyond\z5u\z5u.s
 goto :EOF
+)
 
+set DISK=Pitch Dark.2mg
+
+if "%1" equ "dsk" (
 :dsk
-1>nul copy /y res\"Pitch Dark.master games collection.do.not.edit.2mg" build\%DISK%
+call :asm
+1>nul copy /y res\"Pitch Dark.master games collection.do.not.edit.2mg" "build\%DISK%"
 1>nul copy /y res\WEEGUI build\
 1>nul copy /y res\_FileInformation.txt build\
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\GRUE.SYSTEM"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\PITCH.DARK"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\WEEGUI"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\ONBEYOND.SYSTEM"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\ONBEYONDZ3"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\ONBEYONDZ4"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\ONBEYONDZ5"
-cadius ADDFILE build\%DISK% "/PITCH.DARK/" "build\ONBEYONDZ5U"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\GRUE.SYSTEM"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\PITCH.DARK"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\WEEGUI"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\ONBEYOND.SYSTEM"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\ONBEYONDZ3"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\ONBEYONDZ4"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\ONBEYONDZ5"
+cadius ADDFILE "build\%DISK%" "/PITCH.DARK/" "build\ONBEYONDZ5U"
+if not "%1" equ "" set DISK=
 goto :EOF
+)
 
+if "%1" equ "txt" (
+call :dsk
 :txt
-md text
-python3 bin\textnormalize.py text\*
-cadius ADDFOLDER %disk% "/PITCH.DARK/TEXT" text
+2>nul md build\text
+python bin\textnormalize.py text\*
+cd build
+cadius ADDFOLDER "%DISK%" "/PITCH.DARK/TEXT" text
+cd ..
+if not "%1" equ "" set DISK=
+goto :EOF
+)
+
+if "%1" equ "artwork" (
+call :dsk
+:artwork
+1>nul xcopy /q /y /i res\artwork build\artwork
+cd build
+cadius ADDFOLDER "%DISK%" "/PITCH.DARK/ARTWORK" artwork
+cd ..
+if not "%1" equ "" set DISK=
+goto :EOF
+)
+
+if "%1" equ "all" (
+call :clean
+call :dsk
+call :txt
+call :artwork
+set DISK=
+goto :EOF
+)
+
+echo usage: %0 clean / asm / dsk / txt / artwork / all
 goto :EOF
 
-/*
-make script for Windows environments
-a qkumba monstrosity from 2018-02-20
-requires ACME, CADIUS, Python
-https://sourceforge.net/projects/acme-crossass/
-https://www.brutaldeluxe.fr/products/crossdevtools/cadius/
-https://github.com/mach-kernel/cadius
-https://www.python.org/
-requires ACME, CADIUS, Python to be in path
-*/
+rem make script for Windows environments
+rem a qkumba monstrosity from 2018-02-20
+rem requires ACME, CADIUS, Python
+rem https://sourceforge.net/projects/acme-crossass/
+rem https://www.brutaldeluxe.fr/products/crossdevtools/cadius/
+rem https://github.com/mach-kernel/cadius
+rem https://www.python.org/
+rem requires ACME, CADIUS, Python to be in path
